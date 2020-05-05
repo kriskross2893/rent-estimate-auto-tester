6db3067fc260dd040b50f670b8bec3b94f4afd10
kristofer <kristofer.lora.dev@gmail.com>
2020-05-06 03:28:24 +0800

    Function for use in comp report

--- src/lib/rentEstimate.js
+++ src/lib/rentEstimate.js
@@ -1,7 +1,6 @@
 import db from '../model';
 
 export async function findAllDistinctProperties() {
-  const Op = db.Sequelize.Op;
   const sequelize = db.sequelize;
   try {
     return await db.RentEstimate.findAll({
@@ -19,5 +18,42 @@ export async function findAllDistinctProperties() {
   } catch (error) {
     console.error(error);
     return {};
+  }
+}
+
+export async function findAllDistinctPropertiesWhere({
+  numBeds,
+  numBaths,
+  propertyID
+}) {
+  const sequelize = db.sequelize;
+  const {Op} = db.Sequelize;
+  try {
+    return await db.RentEstimate.findAll({
+      where: {
+        numBaths: {
+          [Op.between]: [numBaths - 0.5, numBaths + 0.5]
+        },
+        numBeds,
+        externalKey: {
+          [Op.ne]: propertyID,
+        },
+      },
+      attributes: [
+        [sequelize.fn('DISTINCT', sequelize.col('comp_name')), 'name'],
+        'longitude',
+        'latitude',
+        'numBeds',
+        'numBaths',
+        'type',
+        'externalKey',
+        'targetRent',
+        'isLeased',
+        'rank',
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
   }
 }
