542693bbbbd35a2cf65cc17aea8f04518ff352d1
kristofer <kristofer.lora.dev@gmail.com>
2020-05-13 16:06:29 +0800

    Decode and verify if user authorization is valid

--- src/authGuard.js
+++ src/authGuard.js
@@ -1,4 +1,5 @@
 import debugLib from 'debug';
+import jwt from 'jsonwebtoken';
 
 const debug = debugLib('rent-estimate:authguard');
 
@@ -7,14 +8,27 @@ const {
   AUTH_TOKEN,
 } = process.env;
 
+const matchUserAndToken = (user, token) => {
+  const [foyerUser, stockroomUser] = AUTH_USER.split(',');
+  const [foyerToken, stockroomToken] = AUTH_TOKEN.split(',');
+
+  if (user === foyerUser && token === foyerToken) {
+    return true;
+  }
+  if (user === stockroomUser && token === stockroomToken) {
+    return true;
+  }
+
+  return false;
+};
+
 const authGuard = async (req, res, next) => {
   debug('AuthGuard', req.url);
 
-  if (req.headers && req.headers['authuser'] && req.headers['authtoken']) {
-    if (
-      req.headers['authtoken'] === AUTH_TOKEN
-      && req.headers['authuser'] === AUTH_USER
-    ) {
+  if (req.headers && req.headers['authorization']) {
+    const [bearer, auth] = req.headers.authorization.split(' ');
+    const {user, token} = await jwt.decode(auth);
+    if (matchUserAndToken(user, token)) {
       next();
       return;
     }
