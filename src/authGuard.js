a80935f937d1c476b75bd0178e6c4ba4213d37ea
kristofer <kristofer.lora.dev@gmail.com>
2020-05-15 17:23:57 +0800

    Verify user using jwt

--- src/authGuard.js
+++ src/authGuard.js
@@ -1,11 +1,13 @@
 import debugLib from 'debug';
 import jwt from 'jsonwebtoken';
+import isEmpty from 'lodash/isEmpty';
 
 const debug = debugLib('rent-estimate:authguard');
 
 const {
   AUTH_USER,
   AUTH_TOKEN,
+  AUTH_SECRET
 } = process.env;
 
 const matchUserAndToken = (user, token) => {
@@ -22,13 +24,38 @@ const matchUserAndToken = (user, token) => {
   return false;
 };
 
+const verifyAuthorization = (authHeader) => {
+  const [foyerSecret, stockroomSecret] = AUTH_SECRET.split(',');
+  try {
+    const token = jwt.verify(authHeader, foyerSecret);
+    if (token && !isEmpty(token)) {
+      return token;
+    }
+  } catch (error) {
+    // do nothing
+  }
+  try {
+    const token = jwt.verify(authHeader, stockroomSecret);
+    if (token && !isEmpty(token)) {
+      return token;
+    }
+  } catch (error) {
+    // do nothing
+  }
+  return {};
+};
+
 const authGuard = async (req, res, next) => {
   debug('AuthGuard', req.url);
 
   if (req.headers && req.headers['authorization']) {
     const [bearer, auth] = req.headers.authorization.split(' ');
-    const {user, token} = await jwt.decode(auth);
-    if (matchUserAndToken(user, token)) {
+    const {user, token} = verifyAuthorization(auth);
+    if (
+      user
+      && token
+      && matchUserAndToken(user, token)
+    ) {
       next();
       return;
     }
